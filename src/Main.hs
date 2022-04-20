fd9169ed364da6d4a4918bc35a72560e5c1ddcb7
chirprush <chirpyboat73@gmail.com>
2022-04-19 20:44:31 -0400

    Added colors and history

--- src/Main.hs
+++ src/Main.hs
@@ -2,45 +2,71 @@ import Text.Printf
 import Data.Char
 import Control.Monad.Fix
 
+data Color = Red | Green | Yellow | Gray
+
+colorEscape :: Color -> String
+colorEscape color =
+  case color of
+    Red -> "\x1b[31m"
+    Green -> "\x1b[32m"
+    Yellow -> "\x1b[33m"
+    Gray -> "\x1b[38;5;242m"
+
+colorString :: Color -> String -> String
+colorString color string = colorEscape color ++ string ++ "\x1b[0m"
+
+printColor :: Color -> String -> IO ()
+printColor color = putStrLn . colorString color
+
+data GameState = GameState { stateLives :: Int, stateResults :: [String] }
+
+startingState :: GameState
+startingState = GameState { stateLives = 6, stateResults = [] }
+
+printState :: GameState -> IO ()
+printState state = do
+  if (length $ stateResults state) /= 0 then do
+    putStrLn ""
+    mapM_ putStrLn $ reverse $ stateResults state
+  else return ()
+  putStrLn $ printf "\nYou have %s live(s) left." (colorString Green $ show $ stateLives state)
+
 data ScoreResult = SWin | SResults String
 
 evaluateGuess :: String -> String -> ScoreResult
-evaluateGuess word guess =
-  if word == guess then
-    SWin
-  else
-    SResults $ map evalPair pairs
+evaluateGuess word guess
+  | word == guess = SWin
+  | otherwise = SResults $ concat $ map (\pair@(_, right) -> colorString (evalPair pair) [right]) pairs
     where pairs = zip word guess
           nonmatches = map (\(first, _) -> first) $ filter (\(left, right) -> left /= right) pairs
           evalPair (left, right)
-            | left == right = 'G'
-            | elem right nonmatches = 'Y'
-            | otherwise = ' '
+            | left == right = Green
+            | elem right nonmatches = Yellow
+            | otherwise = Gray
 
 -- TODO: Maybe verify that the word is in a dictionary?
 validateGuess :: String -> Bool
 validateGuess guess = length guess == 5 && all isAlpha guess
 
 main :: IO ()
-main = flip fix (5 :: Int) $ \loop lives -> do
+main = flip fix startingState $ \loop state -> do
   let word = "murky"
-  case lives > 0 of
+  case stateLives state > 0 of
     False -> do
-      putStrLn $ printf "You ran out of lives! The word was '%s'." word
+      printColor Red $ printf "You ran out of lives! The word was %s." (colorString Green word)
       return ()
     True -> do
-      putStrLn $ printf "You have %d live(s) remaining." lives
+      printState state
       putStrLn "Please input a guess:"
-      guess <- getLine
+      _guess <- getLine
+      let guess = map toLower _guess
       case validateGuess guess of
         False -> do
-          putStrLn "\nPlease input a valid guess.\n"
-          loop lives
+          printColor Red "\nPlease input a valid guess.\n"
+          loop state
         True -> do
           case evaluateGuess word guess of
             SWin -> do
-              putStrLn $ printf "\nYou got it! The word was '%s'.\n" word
+              putStrLn $ printf "\nYou got it! The word was %s." (colorString Green word)
             SResults results -> do
-              putStrLn $ printf "\nGuess: %s" guess
-              putStrLn $ printf "       %s\n" results
-              loop $ lives - 1
+              loop state{ stateLives = stateLives state - 1, stateResults = results:stateResults state }
